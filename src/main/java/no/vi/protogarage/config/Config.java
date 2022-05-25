499c4c6765c3f5cc2b4d006f94cd99f43831808c
Niels <niels.ebbeling@kpnmail.nl>
2022-05-25 15:27:10 +0200

    Userrollen tweaked

--- src/main/java/no/vi/protogarage/config/Config.java
+++ src/main/java/no/vi/protogarage/config/Config.java
@@ -49,9 +49,9 @@ public class Config
 			Labor inspection = new Labor("Inspecteren", "Inspecteren van de auto", 30);
 			inspection.setFixedPriceCost(4500);					//Inspectie kost altijd €45,-
 			Labor wiperLabor = new Labor("Wisser vervangen", "Vervangen van de een ruitenwisser", 15);
-			wiperLabor.addPart(partService.getPartById(4l));	//Wiper heeft 4 als id
+			wiperLabor.addPart(partService.getPartById(4L));	//Wiper heeft 4 als id
 			Labor brakeDiscLabor = new Labor("Remschijf vervangen", "Vervangen van een remschijf", 60);
-			brakeDiscLabor.addPart(partService.getPartById(2l));
+			brakeDiscLabor.addPart(partService.getPartById(2L));
 			
 			laborRepository.save(inspection);
 			laborRepository.save(wiperLabor);
@@ -65,14 +65,14 @@ public class Config
 		return args ->
 		{
 			Reparation inspectionReparation = new Reparation("Inspectie");
-			inspectionReparation.addLabor(laborService.getLaborById(1l));
+			inspectionReparation.addLabor(laborService.getLaborById(1L));
 			
 			Reparation replaceWiper = new Reparation("Ruitenwisser vervangen");
-			replaceWiper.addLabor(laborService.getLaborById(2l));
+			replaceWiper.addLabor(laborService.getLaborById(2L));
 			
 			Reparation replaceFrontBrakeDiscs = new Reparation("Remschijven voor vervangen");
-			replaceFrontBrakeDiscs.addLabor(laborService.getLaborById(3l));
-			replaceFrontBrakeDiscs.addLabor(laborService.getLaborById(3l));
+			replaceFrontBrakeDiscs.addLabor(laborService.getLaborById(3L));
+			replaceFrontBrakeDiscs.addLabor(laborService.getLaborById(3L));
 			
 			reparationRepository.save(inspectionReparation);
 			reparationRepository.save(replaceWiper);
@@ -90,32 +90,28 @@ public class Config
 			mitsubishi.setPayed(false);
 			mitsubishi.setAtShop(true);
 			
-			mitsubishi.addReparation(reparationService.getReparationById(1l));
-			mitsubishi.addReparation(reparationService.getReparationById(2l));
-			mitsubishi.addReparation(reparationService.getReparationById(3l));
+			mitsubishi.addReparation(reparationService.getReparationById(1L));
+			mitsubishi.addReparation(reparationService.getReparationById(2L));
+			mitsubishi.addReparation(reparationService.getReparationById(3L));
 			
 			Car kia = new Car();
 			kia.setRegistration("16-SJ-PZ");
 			kia.setPayed(false);
 			kia.setAtShop(true);
 			
-			kia.addReparation(reparationService.getReparationById(2l));
+			kia.addReparation(reparationService.getReparationById(2L));
 			
 			Car ford = new Car();
 			ford.setRegistration("74-JZB-9");
 			ford.setPayed(true);
 			ford.setAtShop(false);
 			
-			ford.addReparation(reparationService.getReparationById(1l));
-			ford.addReparation(reparationService.getReparationById(3l));
+			ford.addReparation(reparationService.getReparationById(1L));
+			ford.addReparation(reparationService.getReparationById(3L));
 			
 			carRepository.save(mitsubishi);
 			carRepository.save(kia);
 			carRepository.save(ford);
-			
-			//todo weghalen
-			Receipt receipt = new Receipt(mitsubishi);//carService.getCarById(1l));
-			System.out.println(receipt.generate());
 		};
 	}
 	
@@ -124,10 +120,10 @@ public class Config
 	{
 		return args ->
 		{
-			AppUser admin = new AppUser("Niels", "admin", encoder.bCryptPasswordEncoder().encode("admin"), AppUserRole.ADMIN, false, true);
-			AppUser backoffice = new AppUser("Alice", "backoffice", encoder.bCryptPasswordEncoder().encode("pass"), AppUserRole.BACKOFFICE, false, true);
-			AppUser cashier = new AppUser("Anthonie", "cashier", encoder.bCryptPasswordEncoder().encode("pass"), AppUserRole.CASHIER, false, true);
-			AppUser mechanic = new AppUser("Rick", "mechanic", encoder.bCryptPasswordEncoder().encode("pass"), AppUserRole.MECHANIC, false, true);
+			AppUser admin = new AppUser("Arnold", "admin", encoder.bCryptPasswordEncoder().encode("admin"), AppUserRole.ADMIN, false, true);
+			AppUser backoffice = new AppUser("Bob", "backoffice", encoder.bCryptPasswordEncoder().encode("pass"), AppUserRole.BACKOFFICE, false, true);
+			AppUser cashier = new AppUser("Karin", "cashier", encoder.bCryptPasswordEncoder().encode("pass"), AppUserRole.CASHIER, false, true);
+			AppUser mechanic = new AppUser("Mark", "mechanic", encoder.bCryptPasswordEncoder().encode("pass"), AppUserRole.MECHANIC, false, true);
 			
 			appUserRepository.save(admin);
 			appUserRepository.save(backoffice);
--- src/main/java/no/vi/protogarage/security/config/WebSecurityConfig.java
+++ src/main/java/no/vi/protogarage/security/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import no.vi.protogarage.services.AppUserService;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -23,7 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
 {
 	private final AppUserService appUserService;
 	private final BCryptPasswordEncoder bCryptPasswordEncoder;
-
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception
 	{
@@ -31,16 +32,136 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
 				.csrf().disable()
 				.authorizeRequests()
 				
-				//TODO USERROLLEN!!!
-				//todo kijken of dit invloed heeft op /car/nogiets
+				//Customer
+				.antMatchers(PATH_PREFIX + "/customer/**")
+				.hasAnyAuthority(
+						AppUserRole.ADMIN.name(),
+						AppUserRole.CASHIER.name()
+				)
 				
-				.antMatchers(PATH_PREFIX + "/customer/**").hasAnyAuthority(AppUserRole.CASHIER.name(), AppUserRole.ADMIN.name())
-				.antMatchers(PATH_PREFIX + "/car/**").hasAnyAuthority(AppUserRole.ADMIN.name(), AppUserRole.MECHANIC.name())
-				.antMatchers(PATH_PREFIX + "/reparation").hasAnyAuthority(AppUserRole.ADMIN.name())
+				//Car
+				.antMatchers(HttpMethod.GET, PATH_PREFIX + "/car/*")
+				.hasAnyAuthority(
+						AppUserRole.ADMIN.name(),
+						AppUserRole.CASHIER.name(),
+						AppUserRole.MECHANIC.name()
+				)
+				.antMatchers(HttpMethod.POST, PATH_PREFIX + "/car/*")
+				.hasAnyAuthority(
+						AppUserRole.ADMIN.name(),
+						AppUserRole.CASHIER.name()
+				)
+				.antMatchers(HttpMethod.PUT, PATH_PREFIX + "/car/*")
+				.hasAnyAuthority(
+						AppUserRole.ADMIN.name(),
+						AppUserRole.CASHIER.name()
+				)
+				.antMatchers(HttpMethod.DELETE, PATH_PREFIX + "/car/*")
+				.hasAnyAuthority(
+						AppUserRole.ADMIN.name(),
+						AppUserRole.CASHIER.name()
+				)
+				.antMatchers(PATH_PREFIX + "/car/*/cancel")
+				.hasAnyAuthority(
+						AppUserRole.ADMIN.name(),
+						AppUserRole.MECHANIC.name()
+				)
+				.antMatchers(PATH_PREFIX + "/car/*/papers")
+				.hasAnyAuthority(
+						AppUserRole.ADMIN.name(),
+						AppUserRole.CASHIER.name()
+				)
+				
+				//Reparation
+				.antMatchers(HttpMethod.GET, PATH_PREFIX + "/reparation/**")
+				.hasAnyAuthority(
+						AppUserRole.ADMIN.name(),
+						AppUserRole.BACKOFFICE.name(),
+						AppUserRole.CASHIER.name(),
+						AppUserRole.MECHANIC.name()
+				)
+				.antMatchers(HttpMethod.POST, PATH_PREFIX + "/reparation/**")
+				.hasAnyAuthority(
+						AppUserRole.ADMIN.name(),
+						AppUserRole.BACKOFFICE.name(),
+						AppUserRole.MECHANIC.name()
+				)
+				.antMatchers(HttpMethod.PUT, PATH_PREFIX + "/reparation/**")
+				.hasAnyAuthority(
+						AppUserRole.ADMIN.name(),
+						AppUserRole.BACKOFFICE.name()
+				)
+				.antMatchers(HttpMethod.DELETE, PATH_PREFIX + "/reparation/**")
+				.hasAnyAuthority(
+						AppUserRole.ADMIN.name(),
+						AppUserRole.BACKOFFICE.name()
+				)
+				
+				//Labor
+				.antMatchers(HttpMethod.GET, PATH_PREFIX + "/labor/**")
+				.hasAnyAuthority(
+						AppUserRole.ADMIN.name(),
+						AppUserRole.BACKOFFICE.name(),
+						AppUserRole.CASHIER.name(),
+						AppUserRole.MECHANIC.name()
+				)
+				.antMatchers(HttpMethod.POST, PATH_PREFIX + "/labor/**")
+				.hasAnyAuthority(
+						AppUserRole.ADMIN.name(),
+						AppUserRole.BACKOFFICE.name(),
+						AppUserRole.MECHANIC.name()
+				)
+				.antMatchers(HttpMethod.PUT, PATH_PREFIX + "/labor/**")
+				.hasAnyAuthority(
+						AppUserRole.ADMIN.name(),
+						AppUserRole.BACKOFFICE.name()
+				)
+				.antMatchers(HttpMethod.DELETE, PATH_PREFIX + "/labor/**")
+				.hasAnyAuthority(
+						AppUserRole.ADMIN.name(),
+						AppUserRole.BACKOFFICE.name()
+				)
+				
+				//Part
+				.antMatchers(HttpMethod.GET, PATH_PREFIX + "/part/**")
+				.hasAnyAuthority(
+						AppUserRole.ADMIN.name(),
+						AppUserRole.BACKOFFICE.name(),
+						AppUserRole.CASHIER.name(),
+						AppUserRole.MECHANIC.name()
+				)
+				.antMatchers(HttpMethod.POST, PATH_PREFIX + "/part/**")
+				.hasAnyAuthority(
+						AppUserRole.ADMIN.name(),
+						AppUserRole.BACKOFFICE.name(),
+						AppUserRole.MECHANIC.name()
+				)
+				.antMatchers(HttpMethod.PUT, PATH_PREFIX + "/part/**")
+				.hasAnyAuthority(
+						AppUserRole.ADMIN.name(),
+						AppUserRole.BACKOFFICE.name()
+				)
+				.antMatchers(HttpMethod.DELETE, PATH_PREFIX + "/part/**")
+				.hasAnyAuthority(
+						AppUserRole.ADMIN.name(),
+						AppUserRole.BACKOFFICE.name()
+				)
+				
+				//Receipt
+				.antMatchers(PATH_PREFIX + "/receipt/**")
+				.hasAnyAuthority(
+						AppUserRole.ADMIN.name(),
+						AppUserRole.CASHIER.name(),
+						AppUserRole.MECHANIC.name()
+				)
+				
+				//Files
+				.antMatchers(PATH_PREFIX + "/files/**")
+				.hasAnyAuthority(
+						AppUserRole.ADMIN.name()
+				)
 				
 				//.antMatchers(PATH_PREFIX + "").hasAnyAuthority(AppUserRole.ADMIN.name())
-				
-				
 				.anyRequest()
 				.authenticated()
 				.and()
@@ -48,13 +169,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
 				.and()
 				.httpBasic();
 	}
-
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception
 	{
 		auth.authenticationProvider(daoAuthenticationProvider());
 	}
-
+	
 	@Bean
 	public DaoAuthenticationProvider daoAuthenticationProvider()
 	{
