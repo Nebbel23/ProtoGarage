e46fac3d8d7fdfc8dbed6c1a3891af1733ffde63
Niels <niels.ebbeling@kpnmail.nl>
2022-05-25 23:11:13 +0200

    De final commit

--- src/main/java/no/vi/protogarage/ProtoGarageApplication.java
+++ src/main/java/no/vi/protogarage/ProtoGarageApplication.java
@@ -12,23 +12,5 @@ public class ProtoGarageApplication
 	{
 		SpringApplication.run(ProtoGarageApplication.class, args);
 		System.out.println("\n\n" + GARAGE_NAME + " is running!\n");
-		
-		
-		//TODO Git public maken
-		//TODO Git opschonen
-		
-		//TODO alles pdf maken
-		
-		//TODO MariaDB props
-		
-		/*  voor MySQL
-		
-spring.datasource.url= jdbc:mysql://localhost:3306/testdb?useSSL=false
-spring.datasource.username= root
-spring.datasource.password= 123456
-spring.jpa.properties.hibernate.dialect= org.hibernate.dialect.MySQL5InnoDBDialect
-# Hibernate ddl auto (create, create-drop, validate, update)
-spring.jpa.hibernate.ddl-auto= update
-		 */
 	}
 }
